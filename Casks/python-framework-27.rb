a5cdb36824ce9c7019d7fbb2efb220b05b77919b
Min RK <benjaminrk@gmail.com>
2020-02-25 13:17:02 +0100

    2.7.17

--- Casks/python-framework-27.rb
+++ Casks/python-framework-27.rb
@@ -1,6 +1,6 @@
 cask 'python-framework-27' do
-  version '2.7.14'
-  sha256 'd0378ffcc63db4c115ae67856ec8c78d9851c75ca60941aad4b96e794b2744de'
+  version '2.7.17'
+  sha256 '69c5bddef6e2f20a47be63410f890b728281449ed64ea450f89d0e81babc83b4'
 
   url "https://www.python.org/ftp/python/#{version}/python-#{version}-macosx10.6.pkg"
   name 'Python'
