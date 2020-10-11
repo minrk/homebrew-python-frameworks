f047c2927554208fbec20281bda6434051bd6a44
Min RK <benjaminrk@gmail.com>
2020-10-11 12:44:27 +0200

    3.9.0, 3.8.6

--- Casks/python-framework-39.rb
+++ Casks/python-framework-39.rb
@@ -1,6 +1,6 @@
-cask 'python-framework-38' do
-  version '3.8.6'
-  sha256 'c8195f8b056aff380f1e89dc7dd4c37372c37e5163b91c9391f4256bf5b44fe8'
+cask 'python-framework-39' do
+  version '3.9.0'
+  sha256 'dadee1d10c1a8ed58bb64ec7409a48c9fe38599791eaaea29303ee59fb95dfeb'
   url "https://www.python.org/ftp/python/#{version}/python-#{version}-macosx10.9.pkg"
   name 'Python'
   homepage 'https://www.python.org/'
